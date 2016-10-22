0df123cc496cb7ee8b00733b0c9e53cdf2cf1f7a
yougliu <wumumawl@163.com>
2016-10-22 15:07:19 +0800

    modify some error

--- website.js
+++ website.js
@@ -3,8 +3,8 @@ var http = require("http");
 var fs = require("fs");
 
 var server = http.createServer(function(req,res){
-    res.write(200,{'Content-Type' : 'text/html'});
-    fs.fs.createReadStream('./webIndex.html').pipe(res);
+    res.writeHead(200,{'Content-Type' : 'text/html'});
+    fs.createReadStream('./webIndex.html').pipe(res);
 });
 
 server.listen(process.env.PORT || 5000);
